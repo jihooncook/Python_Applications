4f5b172054ac634497302b7aff22c7332d84dac2
Darkness <jihooncook@gmail.com>
2025-01-17 15:50:38 +1000

    Updated the alien_invasion.py file to inclue the First Alien in the game. Had to make changes to alien.py file to fix two bugs.

--- Alien Invasion game/alien.py
+++ Alien Invasion game/alien.py
@@ -6,11 +6,11 @@ class Alien(Sprite):
 
     def __init__(self, ai_game):
         """Initialize the alien and set its starting position."""
-        super().__init__
+        super().__init__()
         self.screen = ai_game.screen
 
         # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load('Alien Invasion game/images/alien.bmp')
         self.rect = self.image.get_rect()
 
         #Start each new alien near the top left of the screen
@@ -19,4 +19,3 @@ class Alien(Sprite):
         
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
-        
