8b224ff0c110c1bfaa757261e642a980167eb00f
Ji Hoon Cook <jihooncook@gmail.com>
2025-03-29 14:06:34 +1000

    Bug fixes

--- Alien Invasion game/settings.py
+++ Alien Invasion game/settings.py
@@ -9,18 +9,15 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship Settings
-        self.ship_speed = 10
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 100
 
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
 
         # How quickly the game speeds up
